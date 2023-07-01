b60497a6f43adb677771bcdc201c4e84ef97c945
Luis Mesajil <lhmesajil@gmail.com>
2023-06-30 21:03:10 -0500

    Update Frames Validations

--- src/frames/dto/frames.dto.ts
+++ src/frames/dto/frames.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, IsNotEmpty, MinLength } from "class-validator";
+
 export class createFrameDto {
+    @IsString()
+    @IsNotEmpty()
     id: string;
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+    
+    @IsString()
     description: string;
 }
